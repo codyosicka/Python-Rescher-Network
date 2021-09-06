0c722d829f0133dd4799a605468a6f945d4e5389
Cody <xosicka@gmail.com>
2021-09-05 22:52:30 -0600

    deleted unused files, fixed a bug in build_causal_network function

--- General/General.py
+++ General/General.py
@@ -714,8 +714,8 @@ def modify_mini_network(original_df, sco, previous_causal_network, name_of_netwo
 
 def build_causal_network():
   
-  #folder_path = r'C:\\Users\\Xaos\\Desktop\\Web App\\causal_networks'
-  folder_path = r'C:\\Users\\Buff14\\Desktop\\Web App\\causal_networks'
+  folder_path = r'C:\\Users\\Xaos\\Desktop\\Web App\\causal_networks'
+  #folder_path = r'C:\\Users\\Buff14\\Desktop\\Web App\\causal_networks'
 
 
   def listDir(dir):
